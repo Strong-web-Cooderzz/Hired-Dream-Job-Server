56ffc128c7e2f8c85bd6959ebf0feff3fe93835a
Abid Hasan <ab1dhasan@outlook.com>
2023-02-05 02:43:59 +0600

    removed duplicate code

--- index.js
+++ index.js
@@ -17,17 +17,6 @@ app.get('/',(req,res)=>{
 
 
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = 'mongodb+srv://DreamUser:7LKaa1qZ3Gh9BYS9@cluster0.b5doc61.mongodb.net/?retryWrites=true&w=majority'
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-const run = async()=>{
-    try{
-        //================= Database all collection part ====================//
-        const jobsCollection = client.db('hired-job').collection('jobs')
-        const usersCollection = client.db('hired-job').collection('users')
-        const applyJobCollection = client.db('hired-job').collection('applyjobs')
-        
-
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
   "mongodb+srv://DreamUser:7LKaa1qZ3Gh9BYS9@cluster0.b5doc61.mongodb.net/?retryWrites=true&w=majority";
