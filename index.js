244f75528a8c620c192d4b6567cd29e30f67f705
Abid Hasan <ab1dhasan@outlook.com>
2023-02-18 22:19:36 +0600

    changed cors

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const cors = require("cors");
 const http = require('http');
 const server = http.createServer(app)
 const { Server } = require('socket.io')
-global.io = new Server(server, {
-	cors: {
-		origin: 'https://hired-dream-job.vercel.app'
-	}
-})
+global.io = new Server(server)
 require("dotenv").config();
 const { getAuth } = require("firebase-admin/auth");
 const { adminApp } = require('./middlewares/verifyJWT');
@@ -53,7 +49,7 @@ app.get('/', (req, res) => {
 	res.send(`Your ip address is ${ip}`);
 });
 
-io.use(cors())
+io.origins('*:*')
 // this verifies connection
 io.use((socket, next) => {
 	const token = socket.handshake.auth.token;
