17b35c84c48164068c86db66b5eb28bf72a31ab9
Md.Ashrafull Islam <ashrafull130@gmail.com>
2023-02-19 02:07:52 +0600

    added res.header for xml request allow

--- index.js
+++ index.js
@@ -6,23 +6,27 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // importing routes
-const candidatesRouter = require('./routes/candidates.route');
-const employersRouter = require('./routes/employers.route');
-const jobsRouter = require('./routes/jobs.route');
-const paymentsRouter = require('./routes/payments.route');
-const usersRouter = require('./routes/users.route');
-const categoryRouter = require('./routes/blogCategory.route');
-const tagsRouter = require('./routes/tags.route');
-const postRoute = require('./routes/blogPosts.route');
-const stripeRoute = require('./routes/stripe.route');
+const candidatesRouter = require("./routes/candidates.route");
+const employersRouter = require("./routes/employers.route");
+const jobsRouter = require("./routes/jobs.route");
+const paymentsRouter = require("./routes/payments.route");
+const usersRouter = require("./routes/users.route");
+const categoryRouter = require("./routes/blogCategory.route");
+const tagsRouter = require("./routes/tags.route");
+const postRoute = require("./routes/blogPosts.route");
+const stripeRoute = require("./routes/stripe.route");
 // const sslPay = require('./routes/sslPay.route');
 
 // middleware
 app.use(cors());
 app.use((req, res, next) => {
-	req.header('Access-Control-Allow-Origin', '*')
+	req.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
 	next();
-})
+});
 app.use(express.json());
 
 // using route
@@ -37,11 +41,11 @@ app.use(postRoute);
 app.use(stripeRoute);
 // app.use(sslPay);
 
-app.get('/', (req, res) => {
-	const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-	res.send(`Your ip address is ${ip}` );
+app.get("/", (req, res) => {
+	const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+	res.send(`Your ip address is ${ip}`);
 });
 
 app.listen(port, () => {
-	console.log('server running on:', port)
-})
+	console.log("server running on:", port);
+});
